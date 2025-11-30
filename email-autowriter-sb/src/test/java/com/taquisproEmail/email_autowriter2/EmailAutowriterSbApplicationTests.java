c5d152f598380c65f8c30c087a504ccc92b1cd7c
Taquispro <121485751+Taquispro@users.noreply.github.com>
2025-11-30 19:54:01 +0530

    Your descriptive commit message here

---
+++ email-autowriter-sb/src/test/java/com/taquisproEmail/email_autowriter2/EmailAutowriterSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taquisproEmail.email_autowriter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAutowriterSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
